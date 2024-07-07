6e6c774d660392f12b26ad9c2fd744c174d3f724
송경호 <s07019@naver.com>
2024-07-07 21:45:20 +0900

    :black_medium_small_square: Ch9. Streaming

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
+const falseFetch = async () => {
+  console.log('Fetching Invoices')
+  await new Promise((resolve)=>{
+    setTimeout(resolve,2000)
+  })
+}
+
+export default async function Page() {
+  await falseFetch()
   return <p>Invoices Page</p>;
 }

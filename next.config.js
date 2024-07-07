1ea6ea2ac566cd0fc3f4f1fa2080889c88b9bc09
송경호 <s07019@naver.com>
2024-07-08 00:02:38 +0900

    :black_medium_small_square: Ch10. Partial Prerendering

--- next.config.js
+++ next.config.js
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+// PPR is experimental currently ⚗️
+// /** @type {import('next').NextConfig} */
+//
+// const nextConfig = {
+//     experimental: {
+//         ppr: 'incremental',
+//     },
+// };
+//
+// export default nextConfig;
